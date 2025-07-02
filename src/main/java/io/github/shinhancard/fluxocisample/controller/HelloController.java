5016a5c768c6071a21f25c8b7ac25610f420d11d
yoojun <ghddbwns9808@gmail.com>
2025-07-02 11:01:44 +0900

    callcounter 함수 추가

--- src/main/java/io/github/shinhancard/fluxocisample/controller/HelloController.java
+++ src/main/java/io/github/shinhancard/fluxocisample/controller/HelloController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static int callcounter = 0;
 
     @GetMapping("/")
     public String hello() {
         return "this is flux test!";
+    }
+
+    @GetMapping("/callcounter")
+    public int funCallCounter(){
+        return ++callcounter;
     }
 }
